483181e8bfde170d9601881a37e33ed1f9730a8f
堀川翔 <horikawashou@horikawashounoMacBook-Pro.local>
2022-09-07 12:57:54 +0900

    implemented mocks from Repository

--- web/src/main/kotlin/com/Sylix/web/repository/PostRepository.kt
+++ web/src/main/kotlin/com/Sylix/web/repository/PostRepository.kt
@@ -6,6 +6,17 @@ import org.springframework.stereotype.Service
 @Service
 class PostRepository {
 
-    fun selectAll() :List<PostEntity> =
-        TODO()
+    fun selectAll() :List<PostEntity> = mock()
+
+
+    // 後から消す
+    private fun mock(): List<PostEntity> {
+        var mocks : MutableList<PostEntity>  = mutableListOf()
+        val mock1 = PostEntity(title = "title1", overview = "概要の説明１")
+        val mock2 = PostEntity(title = "title2", overview = "概要の説明１")
+
+        mocks.add(mock1)
+        mocks.add(mock2)
+        return mocks
+    }
 }
